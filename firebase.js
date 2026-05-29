5c47d2a528d4d2b87ee4d49d1bff2fd1a9cc6ebf
Your Name <innoxoketh@gmail.com>
2026-05-29 07:23:45 +0300

    Update portfolio website and Firebase configuration

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyBFpU7NldfS_b4pCjhStG-nkNhMI_W1uIk",
-authDomain: "portfolio-profile-16519.firebaseapp.com",
-projectId: "portfolio-profile-16519",
-storageBucket: "portfolio-profile-16519.firebasestorage.app",
-messagingSenderId: "486886015305",
-appId: "1:486886015305:web:23d05f3ac64eab483f1a98",
-measurementId: "G-K01957ND5G"
-
+  apiKey: "AIzaSyBFpU7NldfS_b4pCjhStG-nkNhMI_W1uIk",
+  authDomain: "portfolio-profile-16519.firebaseapp.com",
+  projectId: "portfolio-profile-16519",
+  storageBucket: "portfolio-profile-16519.appspot.com",
+  messagingSenderId: "486886015305",
+  appId: "1:486886015305:web:23d05f3ac64eab483f1a98",
+  measurementId: "G-K01957ND5G"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
